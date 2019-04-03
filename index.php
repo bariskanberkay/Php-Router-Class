3ee6b90c9669b6363deb53e6ce3cada1a5027569
bariskanberkay <48731845+bariskanberkay@users.noreply.github.com>
2019-04-03 07:39:56 +0300

    Clear Empty Rows

--- index.php
+++ index.php
@@ -1,40 +1,26 @@
 <?PHP
 
-
-
-
 include('system/route/Route.php');
 
-
-
-
-
-
-
-
-		
 Route::add('/',function(){
-	
+
 	echo "main page";
-	
+
 },['get','post']);
 
 Route::add('/test',function(){
-	
+
 	echo "test page";
-	
+
 	}
 },['get','post']);
 
 
-
-
-
 Route::run('/');
-		
-	
-	
-	
+
+
+
+
 
 
 
